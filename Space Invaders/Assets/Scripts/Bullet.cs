7af4dc16b9f256a9717ef4e14a6779bd5373db3f
Jon Lee <jonlee@Jons-MacBook-Air.local>
2017-09-02 20:41:06 -0400

    init

---
+++ Space Invaders/Assets/Scripts/Bullet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour 
+{
+	private Transform bullet;
+	public float speed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		bullet = GetComponent<Transform> ();
+	}
+
+	void FixedUpdate()
+	{
+		bullet.position += Vector3.up * speed;
+
+		if (bullet.position.y >= 10) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		// Destroy bullet and enemy
+		if (collider.tag == "Enemy") {
+			Destroy (collider.gameObject);
+			Destroy (gameObject);
+
+			// TODO
+			// Increase player score
+		}
+		// Only destroy the bullet 
+		else if (collider.tag == "Base") {
+			Destroy (gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
---
+++ Space Invaders/Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour 
+{
+	private Transform player;
+	public float speed;
+	public float maxBounds;
+	public float minBounds;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = GetComponent<Transform> ();
+	}
+
+	void FixedUpdate () 
+	{
+		float h = Input.GetAxis ("Horizontal");
+
+		// Stops player from going off screen
+		if (player.position.x < minBounds && h < 0) {
+			h = 0;
+		} 
+		else if (player.position.x > maxBounds && h > 0) {
+			h = 0;
+		}
+
+		player.position += Vector3.right * h * speed;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			// Determines when the player is allowed to shoot another bullet
+			nextFire = Time.time + fireRate;
+
+			// Creates the bullet
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+}
