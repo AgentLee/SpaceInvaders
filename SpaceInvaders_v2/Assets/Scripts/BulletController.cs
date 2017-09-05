274dc8023c6085b94e54d2e0d1b6f1a2cbb37757
Jon Lee <agentlee927@gmail.com>
2017-09-05 09:40:25 -0400

    started 3d version

---
+++ SpaceInvaders_v2/Assets/Scripts/BulletController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour 
+{
+	private Transform bullet;
+	public float speed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		bullet = GetComponent<Transform> ();	
+	}
+
+	void FixedUpdate()
+	{
+		bullet.position += Vector3.up * speed;
+
+		// Check
+		if (bullet.position.y >= 11) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.tag == "Enemy") {
+			Destroy (collider.gameObject);
+			Destroy (gameObject);
+
+			// TODO
+			// Increase score
+		}
+		// TODO
+		else if (collider.tag == "Base") {
+			Destroy (gameObject);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
---
+++ SpaceInvaders_v2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	private Transform player;
+	public float speed;
+	public float minBounds;
+	public float maxBounds;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = GetComponent<Transform> ();	
+	}
+	
+	void FixedUpdate () 
+	{
+		float h = Input.GetAxis ("Horizontal");
+
+		if (player.position.x < minBounds && h < 0) {
+			h = 0;
+		} 
+		else if (player.position.x > maxBounds && h > 0) {
+			h = 0;
+		}
+
+		player.position += Vector3.right * h * speed;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+
+			// TODO: FIX!
+			// Need to see if there's a way to spawn bullet without it bouncing off the player.
+			Vector3 pos = shotSpawn.position;
+			pos.y += 1.5f;
+
+			Instantiate (shot, pos, shotSpawn.rotation);
+		}
+	}
+}
